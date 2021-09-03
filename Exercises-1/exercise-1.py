ed16f884da9b891741b5552e066a9c32d7f29907
t9niro00 <t9niro00@students.oamk.fi>
2021-09-03 12:10:29 +0300

    Added few basic exaples of python

---
+++ Exercises-1/exercise-1.py
@@ -0,0 +1,10 @@
+name = "Joe"
+
+print("Good day, sir " + name)
+
+
+sum = 0;
+for x in range (1,5):
+    number = int(input("Give me a number\n"))
+    sum += number
+print("The sum of the numbers is "+str(sum))
