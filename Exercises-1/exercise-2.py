54ef7693b346a37a3b090272ac3a3453b4d3c6be
t9niro00 <t9niro00@students.oamk.fi>
2021-09-03 14:42:43 +0300

    two working sum functions a little differently, another made using an array and the other is made using user input

---
+++ Exercises-1/exercise-2.py
@@ -0,0 +1,19 @@
+a = int(input("Give me a number\n"))
+b = int(input("Give me another number\n"))
+
+print("The first number is " +str(a))
+print("The second number is " +str(b))
+
+def calculateSum(x,y):
+    return a+b
+
+
+c = calculateSum(a,b)
+
+print("Sum of " +str(a) + " and " + str(b) + " is " + str(c))
+
+numbers = [1,2,3,4,5,6,7,8,9]
+
+Sum = sum(numbers)
+
+print("The sum of numbers "+ str(numbers) + " is " + str(Sum))
